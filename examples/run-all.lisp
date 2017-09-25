8fcff932b2968ef7c1ba13a9fbe5aa50540d264b
Will <will.whitty.arbeit@gmail.com>
2017-09-26 00:53:34 +0300

    Enabled all examples

--- examples/run-all.lisp
+++ examples/run-all.lisp
@@ -1,9 +1,8 @@
 (ql:quickload :cl-glfw3-examples)
 
-;(cl-glfw3-examples:events-example)
+(cl-glfw3-examples:events-example)
 (cl-glfw3-examples:basic-window-example)
-;(cl-glfw3-examples:particles-basic-example)
-;(cl-glfw3-examples:fragment-shader-example)
+(cl-glfw3-examples:particles-basic-example)
+(cl-glfw3-examples:fragment-shader-example)
 
-; (quit)
 (format t "~%Welcome to cl-glfw3.~%")
