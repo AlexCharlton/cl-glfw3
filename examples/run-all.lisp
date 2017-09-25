60cd864fb5acab1d7e31d9b17ca733e5fb15a8e6
Will <will.whitty.arbeit@gmail.com>
2017-09-25 23:46:39 +0300

    Added a test script

---
+++ examples/run-all.lisp
@@ -0,0 +1,8 @@
+(ql:quickload :cl-glfw3-examples)
+
+(cl-glfw3-examples:events-example)
+(cl-glfw3-examples:basic-window-example)
+(cl-glfw3-examples:particles-basic-example)
+(cl-glfw3-examples:fragment-shader-example)
+
+(format t "~%Welcome to cl-glfw3.~%")
